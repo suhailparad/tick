c5991594a6cadb7f2aac59abc782f3bfbe852b89
suhailparad <pk.suhail47@gmail.com>
2024-11-06 13:05:59 +0530

    feat: vercel configured in project

--- tick-server/index.js
+++ tick-server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.get('/api/hello', (req, res) => {
   res.send('Hello, World!');
 });
 
